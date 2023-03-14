b5cb9772ef6d4e7b59f94dcf7ddb7fa805a352d8
NotPraneeth <rsphaha@gmail.com>
2023-03-14 20:14:40 +0530

    Running out of time push :run:

--- src/Components/Rightbar/Rightbar.jsx
+++ src/Components/Rightbar/Rightbar.jsx
@@ -6,7 +6,12 @@ const Rightbar = () => {
     <div className='rightbar-wrapper'>
         <div className="rightbar-container">
             <div className="rightbar-main">
-
+              <div className="div">Buy groceries</div>
+              <div className="div">Dinner with Kate</div>
+              <div className="div">Convince IEEE that i'm a good frontend dev</div>
+              <div className="div">Drop them a message saying I have a really good eye for design</div>
+              <div className="div">Work on motion graphics for my client who loves my work</div>
+              <a href='https://notpraneeth.me' target='_blank'>This is my website</a>
             </div>
         </div>
     </div>
